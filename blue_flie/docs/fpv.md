3f886b6a7f039a6212df57b419b696ecdd0bfdae
kamangir <arash@kamangir.net>
2025-03-05 18:40:05 -0800

    ++ - kamangir/bolt#746

--- blue_flie/docs/fpv.md
+++ blue_flie/docs/fpv.md
@@ -5,8 +5,8 @@
 |  |  |  |
 |  | [![image](https://github.com/kamangir/assets/blob/main/blue-flie/fpv/2in-100.png?raw=true)](https://www.youtube.com/watch?v=MxG22nbBNvQ) | [![image](https://github.com/kamangir/assets/blob/main/blue-flie/fpv/35in-160.png?raw=true)](https://www.youtube.com/watch?v=aXrrg48auhU) |
 |  | https://www.youtube.com/watch?v=MxG22nbBNvQ | https://www.youtube.com/watch?v=aXrrg48auhU |
-| size (") |  |  |
-| cost ($) |  |  |
-| weight (g) |  |  |
+| size (") | 2.0 | 3.5 |
+| cost ($) | $100.0 | $160.0 |
+| weight (g) |  | 250.0 |
 |  |  |  |
 
--- blue_flie/fpv.py
+++ blue_flie/fpv.py
@@ -3,42 +3,36 @@ list_of_builds = {
         "url": "https://www.youtube.com/watch?v=MxG22nbBNvQ",
         "size_in": 2.0,
         "cost_dollar": 100.0,
-        "marquee": "2-in-100-dollar",
     },
     "35in-160": {
         "url": "https://www.youtube.com/watch?v=aXrrg48auhU",
         "size_in": 3.5,
         "cost_dollar": 160.0,
         "weight_gr": 250.0,
-        "marquee": "TBA",
     },
     "template": {
         "url": "TBA",
         "size_in": 0.0,
         "cost_dollar": 0.0,
         "weight_gr": 0.0,
-        "marquee": "TBA",
     },
     "template": {
         "url": "TBA",
         "size_in": 0.0,
         "cost_dollar": 0.0,
         "weight_gr": 0.0,
-        "marquee": "TBA",
     },
     "template": {
         "url": "TBA",
         "size_in": 0.0,
         "cost_dollar": 0.0,
         "weight_gr": 0.0,
-        "marquee": "TBA",
     },
     "template": {
         "url": "TBA",
         "size_in": 0.0,
         "cost_dollar": 0.0,
         "weight_gr": 0.0,
-        "marquee": "TBA",
     },
 }
 
@@ -73,10 +67,16 @@ for what in ["marquee", "url", "size_in", "cost_dollar", "weight_gr", "comments"
         elif what == "url":
             items += [list_of_builds[build_name]["url"]]
         elif what == "size_in":
-            items += [""]
+            items += ["{:.1f}".format(list_of_builds[build_name]["size_in"])]
         elif what == "cost_dollar":
-            items += [""]
+            items += ["${:.1f}".format(list_of_builds[build_name]["cost_dollar"])]
         elif what == "weight_gr":
-            items += [""]
+            items += [
+                (
+                    "{:.1f}".format(list_of_builds[build_name]["weight_gr"])
+                    if "weight_gr" in list_of_builds[build_name]
+                    else ""
+                )
+            ]
         elif what == "comments":
             items += [""]
